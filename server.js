6c951afebbac7814304ae3e3f206b3e06df8487e
Andrew Lee <andyrewlee@gmail.com>
2015-07-28 20:10:53 -0700

    Add express

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
-var io = require('socket.io')(process.env.PORT || 5000);
+var express = require('express');
+var path = require('path');
+var app = express();
+var bodyParser = require('body-parser');
 
-io.on('connection', function(socket) {
-  console.log('SERVER::WE ARE USING SOCKETS!');
-  console.log(socket.id);
+app.use(express.static(path.join(__dirname, './static')));
+app.use(bodyParser.urlencoded());
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(req, res) {
+   res.render('index');
 });
 
-process.env.PORT || 5000
+var server = app.listen(app.get('port'), function() {
+    console.log('listening on port', app.get('port'));
+});
+
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function (socket) {
+    console.log('SERVER::WE ARE USING SOCKETS!');
+      console.log(socket.id);
+});
