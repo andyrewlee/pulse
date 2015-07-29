fbf6e4f2dcaad49c2a50c239f7bf47f751a3638b
Andrew Lee <andyrewlee@gmail.com>
2015-07-28 20:52:41 -0700

    finalize

--- server.js
+++ server.js
@@ -14,12 +14,73 @@ app.get('/', function(req, res) {
 });
 
 var server = app.listen(app.get('port'), function() {
-    console.log('listening on port', app.get('port'));
+  console.log('listening on port', app.get('port'));
 });
 
 var io = require('socket.io').listen(server);
 
+var lastFiveResponses = [0,0,0,0,0];
+var lastFiveRespondents = [null,null,null,null,null];
+
+function shiftAndPush(vote, socketId) {
+  lastFiveResponses.shift();
+  lastFiveRespondents.shift();
+
+  lastFiveResponses.push(vote);
+  lastFiveRespondents.push(socketId);
+}
+
+function tooMuchVotingFor(socketId) {
+  var count = 0;
+
+  for(var i=0; i<lastFiveRespondents.length; i++) {
+    if(lastFiveRespondents[i] == socketId) {
+      count++;
+    }
+  }
+
+  if(count > 2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function sumOfResponses() {
+  var sum = 0;
+
+  for(var i=0; i<lastFiveResponses.length; i++) {
+    sum += lastFiveResponses[i];
+  }
+
+  return sum;
+}
+
+function currentPulse() {
+  var sum = sumOfResponses();
+
+  if(sum == 0) {
+    return "green"
+  } else if(sum == 1) {
+    return "yellow"
+  } else if(sum == 2) {
+    return "orange"
+  } else if(sum >= 3) {
+    return "red"
+  }
+}
+
 io.sockets.on('connection', function (socket) {
-    console.log('SERVER::WE ARE USING SOCKETS!');
-      console.log(socket.id);
+  console.log('SERVER::WE ARE USING SOCKETS!');
+
+  socket.on('arrowPressed', function(data) {
+    if(tooMuchVotingFor(socket.id) {
+      console.log("Too much voting for", socket.id);
+    } else {
+      var arrowPressed = data.arrow;
+      lastFiveResponses.shiftAndPush(parseInt(arrowPressed), socket.id);
+      io.sockets.emit(currentPulse());
+      io.sockets.emit("check");
+    }
+  }
 });
